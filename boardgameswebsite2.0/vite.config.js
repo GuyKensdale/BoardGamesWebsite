a25fbac51e2f8a38bb1e09a2fb316ea8eb153a3d
Guy Kensdale <GKensdale@outlook.com>
2024-08-21 20:53:00 +0100

    attempt 3123123123 to fix deploy issues

--- boardgameswebsite2.0/vite.config.js
+++ boardgameswebsite2.0/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
-    base: '/', // Adjust if needed based on your deployment path
+    base: 'boardgameswebsite2.0', // Adjust to match the deployment subdirectory
 });
